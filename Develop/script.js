ab48e0279e12367a9771d96b05fb92a5c7750784
gwarz <gmwarzecha@tutanota.com>
2021-01-16 08:42:17 -0700

    added time to header using moment.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
 
 
 
+// display the current date/day/time at the top of the page
+// color-code each time block to indicate whether it is past/present/future
+  // do this by adding class/removing class if after a time
+  // create a time variable to set this in reference to (use moment.js)
+  // this can all be housed within the same function
+// after entering an event and clicking the save icon, save it in local storage
+// ensure that the saved events presist when the page is refreshed
+
+
+
+
+
+// display current time/date in header
+var currentTime = moment().format("LLLL");
+$("#currentDay").append(currentTime);
